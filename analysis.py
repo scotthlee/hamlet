485444c14904dadb226573db07240c8c94d4f4f0
Scott Lee <yle4@cdc.gov>
2022-06-23 12:14:38 -0400

    switch default CI method to BCA

--- analysis.py
+++ analysis.py
@@ -4,7 +4,7 @@ import pandas as pd
 from importlib import reload
 
 from hamlet.tools import multi as tm
-from hamlet.tools import analysis as ta
+from hamlet.tools import inference as ti
 
 
 # Loading the original data for getting cutpoints based on prevalence
@@ -33,13 +33,13 @@ find_test = pd.read_csv(file_dir + 'findings/stats/test_preds.csv')
 
 
 # Getting the cutpoints
-ab_cuts = ta.get_cutpoint(ab_val.abnormal, 
+ab_cuts = ti.get_cutpoint(ab_val.abnormal, 
                           ab_val.abnormal_prob, 
                           p_adj=ab_p)
-abtb_cuts = ta.get_cutpoint(abtb_val.abnormal,
+abtb_cuts = ti.get_cutpoint(abtb_val.abnormal,
                             abtb_val.abnormal_prob,
                             p_adj=abtb_p)
-find_cuts = ta.get_cutpoints(find_val[find_cols].values,
+find_cuts = ti.get_cutpoints(find_val[find_cols].values,
                              find_val[[s + '_prob' for s in find_cols]].values,
                              column_names=find_cols,
                              p_adj=find_p)
@@ -47,33 +47,27 @@ find_cuts = ta.get_cutpoints(find_val[find_cols].values,
 # Getting the confidence intervals
 ab_j_cis = tm.boot_cis(ab_test.abnormal,
                      ab_test.abnormal_prob,
-                     method='emp',
                      cutpoint=ab_cuts['j'],
                      p_adj=ab_p)
 ab_ct_cis = tm.boot_cis(ab_test.abnormal,
                         ab_test.abnormal_prob,
-                        method='emp',
                         cutpoint=ab_cuts['count_adj'],
                         p_adj=ab_p)
 abtb_j_cis = tm.boot_cis(abtb_test.abnormal,
                          abtb_test.abnormal_prob,
-                         method='emp',
                          cutpoint=abtb_cuts['j'],
                          p_adj=abtb_p)
 abtb_ct_cis = tm.boot_cis(abtb_test.abnormal,
                           abtb_test.abnormal_prob,
-                          method='emp',
                           cutpoint=abtb_cuts['count_adj'],
                           p_adj=abtb_p)
 find_j_cis = [tm.boot_cis(find_test[c],
                           find_test[c + '_prob'],
-                          method='emp',
                           p_adj=find_p.values[i],
                           cutpoint=find_cuts[c]['j'])
               for i, c in enumerate(find_cols)]
 find_ct_cis = [tm.boot_cis(find_test[c],
                            find_test[c + '_prob'],
-                           method='emp',
                            p_adj=find_p.values[i],
                            cutpoint=find_cuts[c]['count_adj'])
                for i, c in enumerate(find_cols)]
