a267ca34075f93a43dba788f4287748a1281ee45
Scott Lee <yle4@cdc.gov>
2022-06-24 08:56:11 -0400

    fix some things with the CI code

--- analysis.py
+++ analysis.py
@@ -13,8 +13,9 @@ find_cols = [
     'infiltrate', 'reticular', 'cavity',
     'nodule', 'pleural_effusion', 'hilar_adenopathy',
     'linear_opacity', 'discrete_nodule', 'volume_loss',
-    'pleural_reaction', 'other', 'miliary'
+    'pleural_reaction', 'other'
 ]
+find_prob_cols = [s + '_prob' for s in find_cols]
 N = samp.shape[0]
 ab_p = np.round(samp.abnormal.sum() / N, 2)
 abtb_p = np.round(samp.abnormal_tb.sum() / N, 2)
@@ -31,7 +32,6 @@ abtb_test = pd.read_csv(file_dir + 'abnormal_tb/stats/test_probs.csv')
 find_val = pd.read_csv(file_dir + 'findings/stats/val_preds.csv')
 find_test = pd.read_csv(file_dir + 'findings/stats/test_preds.csv')
 
-
 # Getting the cutpoints
 ab_cuts = ti.get_cutpoint(ab_val.abnormal, 
                           ab_val.abnormal_prob, 
@@ -43,6 +43,10 @@ find_cuts = ti.get_cutpoints(find_val[find_cols].values,
                              find_val[[s + '_prob' for s in find_cols]].values,
                              column_names=find_cols,
                              p_adj=find_p)
+all_cuts = {'abnormal': ab_cuts, 
+            'abnormal_tb': abtb_cuts, 
+            'findings': find_cuts}
+pickle.dump(all_cuts, open(file_dir + 'cuts.pkl', 'wb'))
 
 # Getting the confidence intervals
 ab_j_cis = tm.boot_cis(ab_test.abnormal,
@@ -71,3 +75,9 @@ find_ct_cis = [tm.boot_cis(find_test[c],
                            p_adj=find_p.values[i],
                            cutpoint=find_cuts[c]['count_adj'])
                for i, c in enumerate(find_cols)]
+
+all_cis = [
+    ab_j_cis, ab_ct_cis, abtb_j_cis,
+    abtb_ct_cis, find_j_cis
+]
+pickle.dump(all_cis, open(file_dir + 'cis.pkl', 'wb'))
