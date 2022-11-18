b02a30508606c24ee2e3a7a8ee177fc080cedbc4
Scott Lee <yle4@cdc.gov>
2022-11-18 14:07:20 -0500

    removing the fine-tuning code for the old B7 model

--- train.py
+++ train.py
@@ -19,12 +19,6 @@ if __name__ == '__main__':
                         default='abnormal',
                         help='Which prediction task to train on.',
                         choices=['abnormal', 'abnormal_tb', 'findings'])
-    parser.add_argument('--training_type',
-                        type=str,
-                        default='base',
-                        choices=['base', 'fine_tune', 'both'],
-                        help='Whether to do base training, fine-tuning, or \
-                        both. Only applies if --mode is either train or both')
     parser.add_argument('--data_dir',
                         type=str,
                         default='C:/Users/yle4/data/',
@@ -53,21 +47,6 @@ if __name__ == '__main__':
                         type=int,
                         default=12,
                         help='Minibatch size for model training and inference.')
-    parser.add_argument('--progressive',
-                        action='store_true',
-                        help='Whether to progressively unfreeze blocks during \
-                        fine-tuning (for models that do not fit in GPU \
-                        memory).')
-    parser.add_argument('--train_all_blocks',
-                        action='store_true',
-                        help='Whether to unfreeze all blocks of the network \
-                        at the beginning of training. Will bypyass fine \
-                        tuning.')
-    parser.add_argument('--starting_block',
-                        type=int,
-                        default=0,
-                        help='For models that do not fit in memory: How many \
-                        blocks to unfreeze at the start of fine-tuning.')
     parser.add_argument('--metric',
                         type=str,
                         default='val_ROC_AUC',
@@ -86,14 +65,9 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     # Parameters
-    BASE_TRAIN = args.training_type in ['base', 'both']
-    FINE_TUNE = args.training_type in ['fine_tune', 'both']
     AUGMENT = not args.no_augmentation
     MODEL_FLAVOR = args.model_flavor
-    ALL_BLOCKS = not args.progressive
     BATCH_SIZE = args.batch_size
-    STARTING_BLOCK = args.starting_block
-    PROGRESSIVE = args.progressive
     TASK = args.task
     METRIC = args.metric
     METRIC_MODE = args.metric_mode
@@ -110,8 +84,6 @@ if __name__ == '__main__':
     # Just some info
     if AUGMENT:
         print('Augmentation on.')
-    if ALL_BLOCKS:
-        print('Training all blocks.\n')
 
     # Setting training strategy
     if DISTRIBUTED:
@@ -197,46 +169,7 @@ if __name__ == '__main__':
     if TRAIN_MOD_FOLDER:
         mod.load_weights(CHECK_DIR + TRAIN_MOD_FOLDER)
 
-    if BASE_TRAIN:
-        mod.fit(train_gen,
-                validation_data=val_gen,
-                callbacks=tr_callbacks,
-                epochs=20)
-
-    if FINE_TUNE and not ALL_BLOCKS:
-        # New callbacks for the fine-tuning phase
-        ft_callbacks = [
-            callbacks.EarlyStopping(patience=1,
-                                    mode=METRIC_MODE,
-                                    monitor=METRIC,
-                                    restore_best_weights=True,
-                                    verbose=1),
-            callbacks.ModelCheckpoint(filepath=CHECK_DIR + 'fine_tuning/',
-                                      save_weights_only=True,
-                                      mode=METRIC_MODE,
-                                      monitor=METRIC,
-                                      save_best_only=True),
-            callbacks.TensorBoard(log_dir=LOG_DIR + 'fine_tuning/')
-        ]
-
-        # Layer numbers for the block breaks
-        b0_blocks = [20, 78, 121, 165, 194]
-        b7_blocks = [64, 258, 406, 555, 659, 763]
-
-        # Dropping the learning rate so things don't blow up
-        K.set_value(mod.optimizer.learning_rate, 4e-4)
-
-        # Progressively fine-tuning the blocks
-        if not PROGRESSIVE:
-            b7_blocks = [b7_blocks[STARTING_BLOCK]]
-            STARTING_BLOCK = 0
-
-        for b in b7_blocks[STARTING_BLOCK:]:
-            for layer in mod.layers[-b:]:
-                if not isinstance(layer, layers.BatchNormalization):
-                    layer.trainable = True
-
-            mod.fit(train_gen,
-                    validation_data=val_gen,
-                    epochs=20,
-                    callbacks=ft_callbacks)
+    mod.fit(train_gen,
+            validation_data=val_gen,
+            callbacks=tr_callbacks,
+            epochs=20)
