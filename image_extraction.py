c78630be44019825740495461eb097190bd6232c
Scott Lee <yle4@cdc.gov>
2023-05-11 16:48:50 -0400

    adding error report toggle WIP

--- image_extraction.py
+++ image_extraction.py
@@ -13,21 +13,35 @@ if __name__ == '__main__':
     parser.add_argument('--dicom_dir',
                         type=str,
                         default='X:/DICOMM/Immigrant/20220201/',
-                        help='directory holding the DICOM files')
+                        help='Directory holding the DICOM files.')
     parser.add_argument('--img_dir',
                         type=str,
                         default='D:/data/hamlet/source/immigrant/',
-                        help='output directory for the image files')
+                        help='Output directory for the image files.')
     parser.add_argument('--prefix',
                         type=str,
                         default='',
-                        help='prefix for the image file names to identify \
-                        which dataset they came from')
+                        help='Prefix for the image file names to identify \
+                        which dataset they came from.')
+    parser.add_argument('--img_dim',
+                        type=int,
+                        default=1024,
+                        help='Desired height or width of the image file.')
     parser.add_argument('--num_files',
                         type=int,
                         default=-1,
-                        help='limit on number of files to process')
+                        help='Limit on number of files to process.')
+    parser.add_argument('--error_report_filename',
+                        type=str,
+                        default='bad_files.csv',
+                        help='Name for the optional error report.')
+    parser.add_argument('--error_report_dir',
+                        type=str,
+                        default=None,
+                        help='Where the error report should be saved.')
     parser.add_argument('--overwrite',
+                        action='store_true')
+    parser.add_arugment('--no_error_report',
                         action='store_true')
     parser.add_argument('--convert_PR',
                         action='store_true')
@@ -44,30 +58,49 @@ if __name__ == '__main__':
     NUM_FILES = args.num_files
     DICOM_DIR = args.dicom_dir
     IMG_DIR = args.img_dir
+    IMG_DIM = args.img_dim
     PREFIX = args.prefix
     OVERWRITE = args.overwrite
+    ERROR_REPORT = not args.no_error_report
+    EFN = args.error_report_filename
+    ER_DIR = IMG_DIR if not args.error_report_dir else args.error_report_dir
 
     # Making the list of files; default is to convert new ones only
     to_convert = [f for f in os.listdir(DICOM_DIR) if ('dcm' in f)
                   or ('dicom' in f)]
-
+    
+    # Option to convert presentation state files
     if not CONVERT_PR:
         to_convert = [f for f in to_convert if '_PR' not in f]
-
+    
+    # Option to overwrite images that have already been extracted
     if not OVERWRITE:
         img_files = [f for f in os.listdir(IMG_DIR)]
         img_files = [f.replace('png', 'dcm') for f in img_files]
         to_convert = np.setdiff1d(to_convert, img_files)
-
+    
+    # Setting the number of files to extract
     if NUM_FILES == -1:
         NUM_FILES = len(to_convert)
+    
+    # Making an empty error report file to write to, if one doesn't exist
+    if ERROR_REPORT:
+        if EFN not in os.listdir(er_dir):
+            report = [''] * 8
+            report_df = pd.DataFrame(report).transpose()
+            report_df.columns = ['group', 'file_name',	'no_TS_ID',
+                                 'unsupported_TS_ID', 'missing_pixels', 
+                                 'corrupt_pixels', 'inverted_colors',
+                                 'brightness_issues']
+            report_df.to_csv(er_dir + EFN, index=False)
 
     files = [f for f in to_convert][:NUM_FILES]
     print(len(files))
 
     with Pool(PROCESSES) as p:
         input = [(f, DICOM_DIR, IMG_DIR,
-                  PREFIX, 1024)
+                  PREFIX, IMG_DIM, True, 
+                  ERROR_REPORT, EFN, ER_DIR)
                  for f in files]
         output = p.starmap(convert_to_png, input)
         p.close()
