0bca72046184b7314ad419ab076dd7f07da42b6f
tjaensch <tjaensch>
2023-05-15 12:10:02 +0000

    Dicom updates from master branch

--- image_extraction.py
+++ image_extraction.py
@@ -1,8 +1,8 @@
 """Batch converts the images in DICOM files to .png."""
 import numpy as np
+import pandas as pd
 import argparse
 import os
-import pandas as pd
 
 from multiprocessing import Pool
 
@@ -51,7 +51,8 @@ if __name__ == '__main__':
                         default=-1,
                         help='number of processes for the Pool')
     parser.set_defaults(overwrite=False,
-                        convert_PR=False)
+                        convert_PR=False,
+                        no_error_report=False)
     args = parser.parse_args()
 
     PROCESSES = args.processes if args.processes != -1 else None
@@ -69,28 +70,28 @@ if __name__ == '__main__':
     # Making the list of files; default is to convert new ones only
     to_convert = [f for f in os.listdir(DICOM_DIR) if ('dcm' in f)
                   or ('dicom' in f)]
-    
+
     # Option to convert presentation state files
     if not CONVERT_PR:
         to_convert = [f for f in to_convert if '_PR' not in f]
-    
+
     # Option to overwrite images that have already been extracted
     if not OVERWRITE:
         img_files = [f for f in os.listdir(IMG_DIR)]
         img_files = [f.replace('png', 'dcm') for f in img_files]
         to_convert = np.setdiff1d(to_convert, img_files)
-    
+
     # Setting the number of files to extract
     if NUM_FILES == -1:
         NUM_FILES = len(to_convert)
-    
+
     # Making an empty error report file to write to, if one doesn't exist
     if ERROR_REPORT:
         if EFN not in os.listdir(ER_DIR):
             report = [''] * 8
             report_df = pd.DataFrame(report).transpose()
             report_df.columns = ['group', 'file_name',	'no_TS_ID',
-                                 'unsupported_TS_ID', 'missing_pixels', 
+                                 'unsupported_TS_ID', 'missing_pixels',
                                  'corrupt_pixels', 'inverted_colors',
                                  'brightness_issues']
             report_df.to_csv(ER_DIR + EFN, index=False)
@@ -100,7 +101,7 @@ if __name__ == '__main__':
 
     with Pool(PROCESSES) as p:
         input = [(f, DICOM_DIR, IMG_DIR,
-                  PREFIX, IMG_DIM, True, 
+                  PREFIX, IMG_DIM, True,
                   ERROR_REPORT, EFN, ER_DIR)
                  for f in files]
         output = p.starmap(convert_to_png, input)
